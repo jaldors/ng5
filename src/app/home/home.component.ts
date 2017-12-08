50562327e03f240e7bb686cf2908568dbce17b14
Jaldors <jaldors@gmail.com>
2017-12-08 13:49:21 -0200

    Interpolation, property binding and event binding with no-inferrable-types=false

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  intCount: number = 3;
+  btnText: string = 'Add an Item';
+  goalText: string = 'My first life goal';
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.intCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.intCount = this.goals.length;
   }
 
 }
